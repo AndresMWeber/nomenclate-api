325dde9654aa0a04c6f384d156faa711805c7230
andresmweber <andresmweber@gmail.com>
2019-06-23 21:25:59 -0400

    updated requirements.  changed to VCS revision isntead of build number as that changes in between circleCI jobs and we want one that persists.

--- nomenclateAPI/app.py
+++ nomenclateAPI/app.py
@@ -1,6 +1,7 @@
 
 import os
 
+from nomenclate import Nom
 from flask import Flask
 from flask_restful import Api
 
@@ -8,12 +9,17 @@ from resources.nomenclate import Nomenclate, NomenclateList
 from resources.token import Token, TokenList
 
 app = Flask(__name__)
+app.nomenclate = Nom()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
-                                                       'sqlite:///data.db')
+@app.route('/render')
+def render_nomenclate():
+    return app.nomenclate.to_json()
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['SECRET_KEY'] = 'andres'
+# app.config['SECRET_KEY'] = 'andres'
 api = Api(app=app)
 
 api.add_resource(Nomenclate, '/nomenclate/<int:_id>')
